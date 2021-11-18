9f2df49484a6a3a880b70832e0bf2e7a8d1abb3b
unknown <leninece.mit@gamil.com>
2021-11-19 01:34:03 +0530

    client

--- app/src/Models/students.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Students {
-    firstname: String,
-    lastname: String,
-    dob: Date,
-    email: { type: String, require: true, unique: true },
-    address: String,
-    gender: Boolean,
-}
